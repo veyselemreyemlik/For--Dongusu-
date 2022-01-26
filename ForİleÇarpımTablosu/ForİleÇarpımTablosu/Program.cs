f92b9db1b8b235477c4b6305c1055224307ffe05
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-01-26 15:12:58 +0300

    Add files via upload

---
+++ ForİleÇarpımTablosu/ForİleÇarpımTablosu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForİleÇarpımTablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Döngüsü İle Çarpım Tablosu
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int a = 0; a <= 10; a++)
+                {
+                    Console.WriteLine(i + "*" + a + "=" + (i * a));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
